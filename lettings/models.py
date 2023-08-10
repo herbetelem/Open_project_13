740edff55429e36fdfd29594fc005e48fa1c3aa9
hlo <herbet.hadrien@gmail.com>
2023-08-10 11:02:13 +0200

    [ADD] adding the app lettings

---
+++ lettings/models.py
@@ -0,0 +1,46 @@
+from django.db import models
+from django.core.validators import MaxValueValidator, MinLengthValidator
+
+
+class Address(models.Model):
+    """
+    A model to represent an address.
+    
+    Attributes:
+        number (PositiveIntegerField): The street number.
+        street (CharField): The name of the street.
+        city (CharField): The city name.
+        state (CharField): The state abbreviation (2 characters).
+        zip_code (PositiveIntegerField): The ZIP code.
+        country_iso_code (CharField): The ISO country code (3 characters).
+    """
+    
+    number = models.PositiveIntegerField(validators=[MaxValueValidator(9999)])
+    street = models.CharField(max_length=64)
+    city = models.CharField(max_length=64)
+    state = models.CharField(max_length=2, validators=[MinLengthValidator(2)])
+    zip_code = models.PositiveIntegerField(validators=[MaxValueValidator(99999)])
+    country_iso_code = models.CharField(max_length=3, validators=[MinLengthValidator(3)])
+
+    def __str__(self):
+        return f'{self.number} {self.street}'
+
+    class Meta:
+            verbose_name = "Addressv"
+            verbose_name_plural = "Address"
+
+
+class Letting(models.Model):
+    """
+    A model to represent a letting property.
+    
+    Attributes:
+        title (CharField): The title of the letting.
+        address (OneToOneField): A reference to the associated address.
+    """
+
+    title = models.CharField(max_length=256)
+    address = models.OneToOneField(Address, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
---
+++ lettings/views.py
@@ -0,0 +1,38 @@
+from django.shortcuts import render
+from .models import Letting
+
+
+
+def lettings_index(request):
+    """
+    View function to display a list of all lettings.
+
+    Parameters:
+        request (HttpRequest): The HTTP request object.
+
+    Returns:
+        HttpResponse: A rendered HTML response with the list of lettings.
+    """
+
+    lettings_list = Letting.objects.all()
+    context = {'lettings_list': lettings_list}
+    return render(request, 'lettings_index.html', context)
+
+def letting(request, letting_id):
+    """
+    View function to display details of a specific letting.
+
+    Parameters:
+        request (HttpRequest): The HTTP request object.
+        letting_id (int): The ID of the letting to display.
+
+    Returns:
+        HttpResponse: A rendered HTML response with the letting details.
+    """
+    
+    letting = Letting.objects.get(id=letting_id)
+    context = {
+        'title': letting.title,
+        'address': letting.address,
+    }
+    return render(request, 'letting.html', context)
