796c758b010c8884f86a873e5697d5121cdee6ce
hlo <herbet.hadrien@gmail.com>
2023-08-10 11:02:42 +0200

    [ADD] adding the app profils

---
+++ profils/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Profile(models.Model):
+    """
+    A model to extend the user profile with additional information.
+    
+    Attributes:
+        user (OneToOneField): A reference to the associated user.
+        favorite_city (CharField): The user's favorite city (optional).
+    """
+    
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    favorite_city = models.CharField(max_length=64, blank=True)
+
+    def __str__(self):
+        return self.user.username
---
+++ profils/views.py
@@ -0,0 +1,34 @@
+from django.shortcuts import render
+from .models import Profile
+
+
+def profiles_index(request):
+    """
+    View function to display a list of all profiles.
+
+    Parameters:
+        request (HttpRequest): The HTTP request object.
+
+    Returns:
+        HttpResponse: A rendered HTML response with the list of profiles.
+    """
+
+    profiles_list = Profile.objects.all()
+    context = {'profiles_list': profiles_list}
+    return render(request, 'profiles_index.html', context)
+
+def profile(request, username):
+    """
+    View function to display details of a specific user profile.
+
+    Parameters:
+        request (HttpRequest): The HTTP request object.
+        username (str): The username of the user whose profile to display.
+
+    Returns:
+        HttpResponse: A rendered HTML response with the profile details.
+    """
+    
+    profile = Profile.objects.get(user__username=username)
+    context = {'profile': profile}
+    return render(request, 'profile.html', context)
