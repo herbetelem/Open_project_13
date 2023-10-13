a734ad27394b7f3a264406c0ec4ecd8dac5f23ad
hlo <herbet.hadrien@gmail.com>
2023-10-13 04:35:56 +0200

    [ADD] add sphinx

--- docs/conf.py
+++ docs/conf.py
@@ -1,30 +1,34 @@
 # Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
 
-# -- Project information
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'Open projet 13'
-copyright = '2021, Graziella'
-author = 'Graziella'
+import os
+import sys
 
-release = '0.1'
-version = '0.1.0'
+sys.path.insert(0, os.path.abspath("../oc_lettings_site"))
 
-# -- General configuration
 
-extensions = [
-    'sphinx.ext.duration',
-    'sphinx.ext.doctest',
-    'sphinx.ext.autodoc',
-    'sphinx.ext.autosummary',
-    'sphinx.ext.intersphinx',
-]
+project = 'Open project 13'
+copyright = '2023, hadrien louppe herbet'
+author = 'hadrien louppe herbet'
+release = '1'
 
-intersphinx_mapping = {
-    'python': ('https://docs.python.org/3/', None),
-    'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
-}
-intersphinx_disabled_domains = ['std']
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = ["sphinx.ext.autodoc","sphinx.ext.viewcode", "ssphinx.ext.napoleon"]
 
 templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
-# - test
+language = 'fr'
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'furo'
+html_static_path = ['_static']
