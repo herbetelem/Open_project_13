14ab5ae5384f1ac06c3166fdc0bb80da1caf6a1d
hlo <herbet.hadrien@gmail.com>
2023-10-13 04:56:35 +0200

    [FIX] fix ortho ssphinx

--- docs/conf.py
+++ docs/conf.py
@@ -10,6 +10,8 @@ import os
 import sys
 
 sys.path.insert(0, os.path.abspath("../oc_lettings_site"))
+sys.path.insert(0, os.path.abspath("../lettings"))
+sys.path.insert(0, os.path.abspath("../profils"))
 
 
 project = 'Open project 13'
