665c9de4add36c530bc70b888471ac9c22857a4d
hlo <herbet.hadrien@gmail.com>
2023-10-13 04:47:21 +0200

    [FIX] fix ortho ssphinx

--- docs/conf.py
+++ docs/conf.py
@@ -30,5 +30,5 @@ language = 'fr'
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'furo'
+html_theme = 'alabaster'
 html_static_path = ['_static']
