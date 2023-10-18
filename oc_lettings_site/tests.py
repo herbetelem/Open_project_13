648958367f059a7ee19038b0a207a455487f4ad6
hlo <herbet.hadrien@gmail.com>
2023-10-18 10:55:08 +0200

    [TEST] test update base template

--- oc_lettings_site/tests.py
+++ oc_lettings_site/tests.py
@@ -1,2 +1,12 @@
 def test_dummy():
     assert 1
+
+from django.test import TestCase
+from django.urls import reverse
+
+class IndexViewTestCase(TestCase):
+    def test_index_view(self):
+        url = reverse('index')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'index.html')
